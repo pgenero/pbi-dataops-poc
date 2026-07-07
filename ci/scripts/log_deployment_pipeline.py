d7a63060d852b7dea2768d3bc43298817a49d628
Pablo Genero <pablo_genero@hotmail.com>
2026-07-07 14:18:14 -0300

    Add artifact id to log

--- ci/scripts/log_deployment_pipeline.py
+++ ci/scripts/log_deployment_pipeline.py
@@ -23,27 +23,24 @@ pl = pipelines.Pipelines(token)
 
 # Function to check the pipeline operation status
 def wait_for_completion(pl, pipeline_id, operation_id, max_retries=15, wait_seconds=10):
+    last_data = None
+
     for i in range(max_retries):
         data = pl.get_pipeline_operation(pipeline_id, operation_id)
-        df = pd.DataFrame(data)
+        last_data = data
 
-        if df.empty:
-            print(f"Attempt {i+1}: empty response")
-            time.sleep(wait_seconds)
-            continue
-
-        status = df["operationStatus"].iloc[0]
+        status = data.get("status")
 
         print(f"Attempt {i+1}: {status}")
 
         if status not in ["NotStarted", "InProgress"]:
             print("✅ Deployment finished")
-            return df
+            return data
 
         time.sleep(wait_seconds)
 
     print("⚠️ Timeout waiting for deployment")
-    return df
+    return last_data
 
 # ========================
 # 2. Loop por target
@@ -74,18 +71,31 @@ for target in targets:
             pipeline_id = os.getenv("OPERATIONS_PIPELINE_ID")
 
         # 2.3 WAIT the end of the Power BI pipeline
-        df = wait_for_completion(pl, pipeline_id, operation_id)
+        pipelineOperationRaw = wait_for_completion(pl, pipeline_id, operation_id)
 
-        log_entry = {
-            "operationId": operation_id,
-            "target": target,
-            "commit": commit,
-            "github_run": github_run_url
-        }
+        pipelineOperationData = []
 
-        print("✅ LOG ENTRY:", log_entry)
+        for step in pipelineOperationRaw.get("executionPlan", {}).get("steps", []):
+            source_target = step.get("sourceAndTarget", {})
 
-        results.append(log_entry)
+            details = {
+                "github_url": github_run_url,
+                "commit": remote_commit,
+
+                "operationId": operation_id,
+                "operationStatus": pipelineOperationRaw.get("status"),
+                "executionStatus": step.get("status"),
+
+                "itemType": source_target.get("type"),
+                "sourceItemId": source_target.get("source"),
+                "sourceItemName": source_target.get("sourceDisplayName"),
+
+                "targetItemId": source_target.get("target"),
+                "targetItemName": source_target.get("targetDisplayName"),
+            }
+
+            pipelineOperationData.append(details)
+
 
     except Exception as e:
         print(f"❌ Error logging {target}: {str(e)}")
